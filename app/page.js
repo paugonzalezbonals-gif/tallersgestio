3a49b03251be8e266f5de3404e8b43147895f33e
paugonzalezbonals-gif <paugonzalezbonals@gmail.com>
2026-06-04 17:46:12 +0200

    Delete app/page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'Arial' }}>
-      <h1>🎵 Escola de Música</h1>
-      <p>Si veus açò, funciona!</p>
-    </div>
-  )
-}
